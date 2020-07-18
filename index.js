bdd4c9dbf131b21cf9eaab7c9e022e9c4de99a09
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-18 18:55:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
-
-
